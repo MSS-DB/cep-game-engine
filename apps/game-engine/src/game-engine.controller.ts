08dcf890cd7c3c7c538bd973699106d086173be8
Teoh Kheng Hong <tkhenghong@gmail.com>
2024-09-09 16:18:07 +0800

    Updated game engine controller swagger documentations

--- apps/game-engine/src/game-engine.controller.ts
+++ apps/game-engine/src/game-engine.controller.ts
@@ -21,7 +21,14 @@ import {
   UpdateGameRequest,
 } from './dtos';
 import { AllExceptionsFilter } from './game-engine.exceptions.filter';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import {
+  ApiBody,
+  ApiOperation,
+  ApiParam,
+  ApiQuery,
+  ApiResponse,
+  ApiTags,
+} from '@nestjs/swagger';
 
 @ApiTags('game-engine')
 @Controller('games')
@@ -32,12 +39,45 @@ export class GameEngineController {
   constructor(private readonly gameEngineService: GameEngineService) {}
 
   @Get(':id')
+  @ApiOperation({ summary: 'Retrieve a game by its ID' })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    description: 'The ID of the game to retrieve',
+  })
+  @ApiResponse({ status: 200, description: 'The game details', type: Game })
+  @ApiResponse({ status: 404, description: 'Game not found' })
   getGame(@Param('id', ParseIntPipe) id: number): Promise<Game> {
     return this.gameEngineService.getGame(id);
   }
 
   @Get()
   @ApiOperation({ summary: 'Get all games with optional filtering' })
+  @ApiQuery({
+    name: 'page',
+    required: false,
+    type: Number,
+    description: 'Page number for pagination',
+  })
+  @ApiQuery({
+    name: 'limit',
+    required: false,
+    type: Number,
+    description: 'Number of items per page',
+  })
+  @ApiQuery({
+    name: 'sortBy',
+    required: false,
+    type: String,
+    description: 'Field to sort by',
+  })
+  @ApiQuery({
+    name: 'sortOrder',
+    required: false,
+    type: String,
+    enum: ['ASC', 'DESC'],
+    description: 'Order of sorting, either ASC or DESC',
+  })
   @ApiResponse({ status: 200, description: 'List of games', type: [Game] })
   getGames(@Query() filter: GamePaginateRequest): Promise<Game[]> {
     return this.gameEngineService.getGames(filter);
@@ -45,6 +85,10 @@ export class GameEngineController {
 
   @Post()
   @ApiOperation({ summary: 'Create a new game' })
+  @ApiBody({
+    type: CreateGameRequest,
+    description: 'Details of the new game to create',
+  })
   @ApiResponse({ status: 201, description: 'The created game', type: Game })
   createGame(@Body() createGameRequest: CreateGameRequest): Promise<Game> {
     return this.gameEngineService.createGame({
@@ -56,7 +100,17 @@ export class GameEngineController {
 
   @Put(':id')
   @ApiOperation({ summary: 'Update an existing game by ID' })
-  @ApiResponse({ status: 204, description: 'The game has been updated' })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    description: 'The ID of the game to update',
+  })
+  @ApiBody({
+    type: UpdateGameRequest,
+    description: 'Updated details of the game',
+  })
+  @ApiResponse({ status: 200, description: 'The game has been updated' })
+  @ApiResponse({ status: 404, description: 'Game not found' })
   updateGame(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateGameRequest: UpdateGameRequest,
@@ -66,6 +120,10 @@ export class GameEngineController {
 
   @Post('instances')
   @ApiOperation({ summary: 'Create a new game instance' })
+  @ApiBody({
+    type: CreateGameInstanceRequest,
+    description: 'Details of the new game instance to create',
+  })
   @ApiResponse({
     status: 201,
     description: 'The created game instance',
@@ -85,10 +143,20 @@ export class GameEngineController {
 
   @Put('instances/:id')
   @ApiOperation({ summary: 'Update an existing game instance by ID' })
+  @ApiParam({
+    name: 'id',
+    type: Number,
+    description: 'The ID of the game instance to update',
+  })
+  @ApiBody({
+    type: UpdateGameInstanceRequest,
+    description: 'Updated details of the game instance',
+  })
   @ApiResponse({
     status: 204,
     description: 'The game instance has been updated',
   })
+  @ApiResponse({ status: 404, description: 'Game instance not found' })
   updateGameInstance(
     @Param('id', ParseIntPipe) id: number,
     @Body() updateGameInstanceRequest: UpdateGameInstanceRequest,
